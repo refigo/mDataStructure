3ad98f8b75436502f060f321fa74aca6bad5fa62
refigo <wlguq100@naver.com>
2022-06-06 14:14:56 +0900

    init: graph MST Kruskal

---
+++ Graph/MST/Kruskal/main.c
@@ -0,0 +1,16 @@
+#include "kruskal.h"
+
+
+// run kruskal
+// sort edge 
+
+int main(void)
+{
+	// set graph
+
+	// run Kruskal algorithm
+
+	// display MST(minimum spanning tree)
+
+	return (0);
+}
